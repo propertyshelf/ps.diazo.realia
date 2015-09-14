d5af3b6049dcd53d0a6f18d480fddea2db777f62
Jens Krause <jens@propertyshelf.com>
2015-09-14 14:02:58 +0700

    count up dev nr.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 
 from setuptools import setup, find_packages
 
-version = '0.7.dev1'
+version = '0.7.dev2'
 description = 'Plone Realia Theme'
 long_description = ('\n'.join([
     open('README.rst').read(),
