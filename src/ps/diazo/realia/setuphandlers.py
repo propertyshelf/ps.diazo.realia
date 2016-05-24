c6d959bb88a0ef6e5dec427e3382ddc3523f2038
Thomas Massmann <thomas@propertyshelf.com>
2016-05-24 08:36:50 +0200

    Uninstall GS profile should be hidden.

--- src/ps/diazo/realia/setuphandlers.py
+++ src/ps/diazo/realia/setuphandlers.py
@@ -1,6 +1,20 @@
 # -*- coding: utf-8 -*-
 """Post install import steps for ps.diazo.realia."""
 
+# zope imports
+from Products.CMFPlone.interfaces import INonInstallable
+from zope.interface import implementer
+
+
+@implementer(INonInstallable)
+class HiddenProfiles(object):
+
+    def getNonInstallableProfiles(self):
+        """Do not show on Plone's list of installable profiles."""
+        return [
+            u'ps.diazo.realia:uninstall',
+        ]
+
 
 def is_not_current_profile(context):
     return context.readDataFile(
