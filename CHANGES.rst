81b1bb1e70b52d21bd44eb641d977fea5338fb74
Thomas Massmann <thomas@propertyshelf.com>
2017-01-23 09:54:25 +0100

    Preparing release 0.10

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,7 +1,7 @@
 Changelog
 =========
 
-0.10 (unreleased)
+0.10 (2017-01-23)
 -----------------
 
 - Fix blank page issue when switching orientation on mobile devices.
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import (
     setup,
 )
 
-version = '0.10.dev0'
+version = '0.10'
 description = 'Propertyshelf Realia Theme'
 long_description = ('\n'.join([
     open('README.rst').read(),
