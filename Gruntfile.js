65343a667597664314fe23d5f2e295f785fd5592
Thomas Massmann <thomas@propertyshelf.com>
2017-08-25 12:16:59 +0200

    Add grunt files.

---
+++ Gruntfile.js
@@ -0,0 +1,118 @@
+module.exports = function (grunt) {
+    'use strict';
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        // we could just concatenate everything, really
+        // but we like to have it the complex way.
+        // also, in this way we do not have to worry
+        // about putting files in the correct order
+        // (the dependency tree is walked by r.js)
+        less: {
+            dist: {
+                options: {
+                    compress: true,
+                    paths: [],
+                    strictMath: false,
+                    sourceMap: true,
+                    outputSourceFiles: true,
+                    sourceMapURL: '++theme++ps.diazo.realia/assets/css/realia.css.map',
+                    sourceMapFilename: 'assets/css/realia.css.map',
+                    modifyVars: {
+                        "isPlone": "false"
+                    }
+                },
+                files: {
+                    'assets/css/realia.css': 'less/realia.less',
+                    'assets/css/realia-blue.css': 'less/realia-blue.less',
+                    'assets/css/realia-brown-dark.css': 'less/realia-brown-dark.less',
+                    'assets/css/realia-brown.css': 'less/realia-brown.less',
+                    'assets/css/realia-gray-blue.css': 'less/realia-gray-blue.less',
+                    'assets/css/realia-gray-brown-dark.css': 'less/realia-gray-brown-dark.less',
+                    'assets/css/realia-gray-brown.css': 'less/realia-gray-brown.less',
+                    'assets/css/realia-gray-green-light.css': 'less/realia-gray-green-light.less',
+                    'assets/css/realia-gray-green.css': 'less/realia-gray-green.less',
+                    'assets/css/realia-gray-magenta.css': 'less/realia-gray-magenta.less',
+                    'assets/css/realia-gray-orange.css': 'less/realia-gray-orange.less',
+                    'assets/css/realia-gray-red.css': 'less/realia-gray-red.less',
+                    'assets/css/realia-gray-turquiose.css': 'less/realia-gray-turquiose.less',
+                    'assets/css/realia-gray-violet.css': 'less/realia-gray-violet.less',
+                    'assets/css/realia-green-light.css': 'less/realia-green-light.less',
+                    'assets/css/realia-green.css': 'less/realia-green.less',
+                    'assets/css/realia-magenta.css': 'less/realia-magenta.less',
+                    'assets/css/realia-orange.css': 'less/realia-orange.less',
+                    'assets/css/realia-red.css': 'less/realia-red.less',
+                    'assets/css/realia-turquiose.css': 'less/realia-turquiose.less',
+                    'assets/css/realia-violet.css': 'less/realia-violet.less',
+                }
+            }
+        },
+        postcss: {
+            options: {
+                map: true,
+                processors: [
+                    require('autoprefixer')({
+                        browsers: ['last 2 versions']
+                    })
+                ]
+            },
+            dist: {
+                src: 'assets/css/*.css'
+            }
+        },
+        watch: {
+            scripts: {
+                files: [
+                    'less/*.less'
+                ],
+                tasks: ['less', 'postcss']
+            }
+        },
+        browserSync: {
+            html: {
+                bsFiles: {
+                    src : [
+                      'less/*.less'
+                    ]
+                },
+                options: {
+                    watchTask: true,
+                    debugInfo: true,
+                    online: true,
+                    server: {
+                        baseDir: "."
+                    },
+                }
+            },
+            plone: {
+                bsFiles: {
+                    src : [
+                      'less/*.less'
+                    ]
+                },
+                options: {
+                    watchTask: true,
+                    debugInfo: true,
+                    proxy: "localhost:8080",
+                    reloadDelay: 3000,
+                    // reloadDebounce: 2000,
+                    online: true
+                }
+            }
+        }
+    });
+
+
+    // grunt.loadTasks('tasks');
+    grunt.loadNpmTasks('grunt-browser-sync');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-postcss');
+
+    // CWD to theme folder
+    grunt.file.setBase('./src/ps/diazo/realia/diazo_resources');
+
+    grunt.registerTask('compile', ['less', 'postcss']);
+    grunt.registerTask('default', ['compile']);
+    grunt.registerTask('bsync', ["browserSync:html", "watch"]);
+    grunt.registerTask('plone-bsync', ["browserSync:plone", "watch"]);
+};
