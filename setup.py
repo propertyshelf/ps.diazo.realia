853fa5fdfdf358d8aa7ca1db59f05202db82c485
Thomas Massmann <thomas@propertyshelf.com>
2017-08-30 10:46:25 +0200

    Back to development: 0.15

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import (
     setup,
 )
 
-version = '0.14'
+version = '0.15.dev0'
 description = 'Propertyshelf Realia Theme'
 long_description = ('\n'.join([
     open('README.rst').read(),
